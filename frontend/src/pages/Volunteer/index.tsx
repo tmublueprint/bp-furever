9daaa28dd33d380c38bd101a366f6e649f02d786
davidxk3 <davidxk3@gmail.com>
2026-02-21 20:07:15 -0500

    Added Volunteer Roles & Opportunities section (#54)

--- frontend/src/pages/Volunteer/index.tsx
+++ frontend/src/pages/Volunteer/index.tsx
@@ -1,4 +1,7 @@
 import './style.css';
+import Role1 from '../../assets/on-site-support.svg'; 
+import Role2 from '../../assets/foster-care.svg'; 
+import Role3 from '../../assets/driver-support.svg'; 
 
 function Volunteer() {
   return (
@@ -30,6 +33,40 @@ function Volunteer() {
               
             </div>
           </div>
+        </section>
+
+        <section className="volunteer-roles-container">
+          <h1 className="volunteer-roles-title">Volunteer Roles & Opportunities</h1>
+          <div className="volunteer-roles">
+            <div className="volunteer-role">
+              <img src={Role1} alt ="house icon"/>
+              <h1>On-Site Support</h1>
+              <p>
+                Lorem ipsum dolor sit amet,
+                consectetuer adipiscing elit, 
+                sed diam nonummy nibh eu 
+              </p>
+            </div>
+            <div className="volunteer-role">
+              <img src={Role2} alt ="baby bottle icon"/>
+              <h1>Foster Care</h1>
+              <p>
+                Lorem ipsum dolor sit amet,
+                consectetuer adipiscing elit, 
+                sed diam nonummy nibh eu 
+              </p>
+            </div>
+            <div className="volunteer-role">
+              <img src={Role3} alt ="car icon"/>
+              <h1>Driver Support</h1>
+              <p>
+                Lorem ipsum dolor sit amet,
+                consectetuer adipiscing elit, 
+                sed diam nonummy nibh eu 
+              </p>
+            </div>
+          </div>
+          <div className="green-vector"></div>
         </section>
       </main>
     </>
