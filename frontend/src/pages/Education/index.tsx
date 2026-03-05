bcd281da4cd119bf35c13f6f08fa373b0783f9d8
Dylan <dylan.huynh@torontomu.ca>
2026-03-05 14:52:12 -0500

    Added guides title

---
+++ frontend/src/pages/Education/index.tsx
@@ -0,0 +1,22 @@
+import './style.css';
+import NavBar from '../../components/NavBar/NavBar';
+import Footer from '../../components/Footer/Footer';
+
+function Education() {
+  return (
+    <>
+    <main className="education-page">
+      <NavBar />
+       <section className="education-hero">
+         <div className="education-hero-container">
+           <h1 className="education-hero-title">Wildlife Information & Guides</h1>
+           <p className="education-hero-body">Practical guidance to help protect wildlife and coexist responsibly.</p>
+         </div>
+       </section>
+      <Footer />
+     </main>
+    </>
+  )
+}
+
+export default Education
