987ac928a060630aa930d06c2c272121ed0fb4e1
Zachary TN <zacharytoyenakamura@gmail.com>
2026-03-17 01:09:53 -0400

    bad admin UI

--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,10 +1,38 @@
 import './style.css';
+import PDFcard from '../../components/PDFCard/PDFCard';
 
 function Admin() {
   return (
     <>
       <p>Testing Admin Side!</p>
-      {/* Add code here */}
+      <div id="admin-container">
+        <div id="admin-input-container">
+          <div>
+            <p>Cover Image</p>
+            <input type="file" accept="image/*"></input>
+          </div>
+          <div id="title-input-container">
+            <p>Title</p>
+            <input type="text" style={{height:16,margin:"auto 0 auto 10px"}}></input>
+          </div>
+          <div id="summary-input-container">
+            <p>Summary</p>
+            <input type="text" style={{height:16,margin:"auto 0 auto 10px"}}></input>
+          </div>
+          <div>
+            <p>PDF</p>
+            <input type="file" accept="application/pdf"></input>
+          </div>
+          <p>Or</p>
+          <div>
+            <p>Hyperlink</p>
+            <input type="text" accept="application/pdf"></input>
+          </div>
+        </div>
+        <div id="admin-preview-container">
+          <PDFcard image="" title="" summary='' link=""/>
+        </div>
+      </div>
     </>
   )
 }
