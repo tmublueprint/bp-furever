3639e498dfe705bc42c8dbc8c461592e2223352b
Ratanatepy Narin <ratanatepyn@gmail.com>
2026-03-25 22:32:15 -0400

    fix ids syntax in pdfcard to className

--- frontend/src/components/PDFCard/PDFCard.tsx
+++ frontend/src/components/PDFCard/PDFCard.tsx
@@ -15,18 +15,18 @@ function PDFCard({image, title, summary, link}: PDFCardProps){
     const hasCustomImage = Boolean(image);
 
     return(
-        <div id="PDFCard">
-            <div id="pdf-card-image-container">
-                <img id="pdf-card-image" src={image || placeholder} alt=""></img>
-                {!hasCustomImage && <img id="pdf-card-overlayed-img" src={missingImg}></img>}
+        <div className="PDFCard">
+            <div className="pdf-card-image-container">
+                <img className="pdf-card-image" src={image || placeholder} alt=""></img>
+                {!hasCustomImage && <img className="pdf-card-overlayed-img" src={missingImg}></img>}
             </div>
-            <div id="pdf-card-content">
+            <div className="pdf-card-content">
                 <h1>{title}</h1>
                 <p>{summary}</p>
-                <a id="pdf-card-btn" href={link} target="_blank" rel="noopener noreferrer">Read Guide
-                    <div id="pdf-btn-imgs">
-                        <img id="pdf-card-arrow" src={arrow} alt="Arrow"></img>
-                        <img id="pdf-card-ellipse" src={ellipse}></img>
+                <a className="pdf-card-btn" href={link} target="_blank" rel="noopener noreferrer">Read Guide
+                    <div className="pdf-btn-imgs">
+                        <img className="pdf-card-arrow" src={arrow} alt="Arrow"></img>
+                        <img className="pdf-card-ellipse" src={ellipse}></img>
                     </div>
                 </a>
             </div>
