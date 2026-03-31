9a9e6bf65ed17f3c1d26d04e79d199b19bfc073b
Zachary TN <zacharytoyenakamura@gmail.com>
2026-03-31 14:35:22 -0400

    Renamed a few things

--- frontend/src/pages/Education/index.tsx
+++ frontend/src/pages/Education/index.tsx
@@ -3,19 +3,19 @@ import NavBar from '../../components/NavBar/NavBar';
 import Footer from '../../components/Footer/Footer';
 import PDFGallery from '../../components/PDFGallery/PDFGallery';
 import wildlife from "../../assets/pdf/Relocating Wildlife Doesn't Work.pdf";
-import property from '../../assets/pdf/Wildlife proofing your property_ A checklist for your home - Oakville.pdf';
+import property from '../../assets/pdf/Wildlife proofing your property A checklist for your home - Oakville.pdf';
 
 const pdfData = [
   {
     id: 1, //to be change when needed
-    image: "src/assets/image/wildlife-proofing.png",
+    image: "src/assets/pdf/wildlife-proofing.png",
     title: "Wildlife proofing your property",
     summary: "A checklist for your home",
     link: property,
   },
   {
     id: 2,
-    image: "src/assets/image/relocating-wildlife.png",
+    image: "src/assets/pdf/relocating-wildlife.png",
     title: "Relocating Wildlife Doesn't Work",
     summary: "Why relocation is not an effective solution",
     link: wildlife,
