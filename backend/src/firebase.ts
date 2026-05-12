ba593c95010c7927a9b284969e71f12e70adb0d5
longrict <125839240+longrict@users.noreply.github.com>
2026-05-12 03:14:48 -0400

    firebase emulator setup

---
+++ backend/src/firebase.ts
@@ -0,0 +1,23 @@
+import admin from 'firebase-admin';
+
+const PROJECT_ID = 'demo-project';
+const EMULATOR_HOST = 'firebase-emulator:3003'; 
+const AUTH_EMULATOR_HOST = 'firebase-emulator:9099'
+const CLOUD_STORAGE_HOST = 'firebase-emulator:9100';
+
+
+// emulator, not live firebase services
+if (!admin.apps.length) {
+  process.env.GOOGLE_CLOUD_PROJECT = PROJECT_ID;       
+  process.env.FIRESTORE_EMULATOR_HOST = EMULATOR_HOST; 
+  process.env.FIREBASE_AUTH_EMULATOR_HOST = AUTH_EMULATOR_HOST;
+  process.env.FIREBASE_STORAGE_EMULATOR_HOST = CLOUD_STORAGE_HOST;
+
+  admin.initializeApp({
+    projectId: PROJECT_ID,
+  } as any);
+}
+
+export const bucket = admin.storage().bucket();
+export const db = admin.firestore();
+export default admin
