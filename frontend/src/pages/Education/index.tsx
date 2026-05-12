9140f025347c30516c8eb9647bcf777a04ef3a62
Asha Afroze <197552642+asha-afroze@users.noreply.github.com.>
2026-05-11 21:18:35 -0400

    edit html tags for education page

--- frontend/src/pages/Education/index.tsx
+++ frontend/src/pages/Education/index.tsx
@@ -25,19 +25,20 @@ const pdfData = [
 function Education() {
   return (
     <>
+    <NavBar />
     <main className="education-page">
-      <NavBar />
        <section className="education-hero">
          <div className="education-hero-container">
            <h1 className="education-hero-title">Wildlife Information & Guides</h1>
            <p className="education-hero-body">Practical guidance to help protect wildlife and coexist responsibly.</p>
          </div>
        </section>
+
        <section className='pdf-section'>
         <PDFGallery pdfList={pdfData} />
-       </section>
-      <Footer />
-     </main>
+       </section> 
+    </main>
+    <Footer />
     </>
   )
 }
