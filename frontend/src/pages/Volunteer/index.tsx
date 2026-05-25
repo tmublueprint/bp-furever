6d76dd6c6213182c3b773f5b021cd9c3228f8932
Ratanatepy Narin <ratanatepyn@gmail.com>
2026-05-25 13:35:03 -0400

    replaced raw code with component

--- frontend/src/pages/Volunteer/index.tsx
+++ frontend/src/pages/Volunteer/index.tsx
@@ -42,58 +42,6 @@ function Volunteer() {
             </div>
           </div>
         </section>
-
-        {/* <section className="volunteer-roles-container">
-          <h2 className="volunteer-roles-title">Volunteer Roles & Opportunities</h2>
-          <div className="volunteer-roles">
-            <div className="volunteer-role">
-              <img src={Role1} alt ="house icon"/>
-              <h3>On-Site Support</h3>
-              <p>
-                Lorem ipsum dolor sit amet,
-                consectetuer adipiscing elit, 
-                sed diam nonummy nibh eu 
-              </p>
-            </div>
-            <div className="volunteer-role">
-              <img src={Role2} alt ="baby bottle icon"/>
-              <h3>Foster Care</h3>
-              <p>
-                Lorem ipsum dolor sit amet,
-                consectetuer adipiscing elit, 
-                sed diam nonummy nibh eu 
-              </p>
-            </div>
-            <div className="volunteer-role">
-              <img src={Role3} alt ="car icon"/>
-              <h3>Driver Support</h3>
-              <p>
-                Lorem ipsum dolor sit amet,
-                consectetuer adipiscing elit, 
-                sed diam nonummy nibh eu 
-              </p>
-            </div>
-          </div>
-          <div className="green-vector"></div>
-        </section>
-
-        <section className="volunteer-requirements-container">
-          <h2 className="volunteer-requirements-title">General Volunteer Requirements</h2>
-          <div className="volunteer-requirements">
-            <ul className="volunteer-requirements-list">
-              <li className="volunteer-requirements-item">✔ Available during baby season (minimum <span style={{ fontWeight: 600 }}>2× per week, 4-hour shifts</span>)</li>
-              <li className="volunteer-requirements-item">✔ Valid driver's license</li>
-              <li className="volunteer-requirements-item">✔ RVS exam completed (MNRF - Aylmer District) *Volunteers and Fosters*</li>
-              <li className="volunteer-requirements-item">✔ Must be 18 years or older</li>
-              <li className="volunteer-requirements-item">✔ Resume required (no cover letter) plus 2 references</li>
-              <li className="volunteer-requirements-item">✔ Tetanus and Season Flu Vaccines</li>
-              <li className="volunteer-requirements-item" style={{ fontStyle: "italic" }}>— Experience with wildlife or animal rescue is considered an asset.</li>
-            </ul>
-          </div>
-          <div className="volunteer-requirements-vector">
-            <h1 className="volunteer-requirements-vector-text">Wildlife rehabilitation is a long-term commitment, especially during baby season.<br />Volunteers must be dependable and available as scheduled.</h1>
-          </div>
-        </section> */}
         <VolunteerSection/>
         <section className="how-to-apply">
           <div className="container-how-to-apply">
