0d6f1a1f16c10fbf2cffb7c6e930941b8163bbf5
Ratanatepy Narin <ratanatepyn@gmail.com>
2026-06-20 15:33:35 -0400

    added protectedRoute for admin page, and admin login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,21 +7,32 @@ import Landing from './pages/LandingPage';
 import Volunteer from './pages/Volunteer';
 import ErrorPage from './pages/ErrorPage';
 import ContactUs from './pages/ContactUs';
+import { ProtectedRoute } from './components/ProtectRoute/ProtectedRoute';
+import AdminLogin from './pages/AdminLogin';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/admin" element={<Admin/>} /> {/* Do not leave this public! */}
-        <Route path="/facebook" element={<Facebook/>} />
-        <Route path="/foundAnAnimal" element={<FoundAnimal/>} />
-        <Route path="/education" element={<Education/>} />
-        <Route path="/" element={<Landing/>} />
-        <Route path="/volunteer" element={<Volunteer/>} />
-        <Route path="/contact-us" element={<ContactUs/>} /> 
-        <Route path="*" element={<ErrorPage />} /> {/* Any other page which is not found above */}
-      </Routes>
-
+      <AuthProvider>
+        <Routes>
+          <Route path="/admin/login" element={<AdminLogin/>}/>
+          <Route 
+            path="/admin"
+            element={
+            <ProtectedRoute>
+              <Admin/>
+            </ProtectedRoute>
+          }/>
+          <Route path="/facebook" element={<Facebook/>} />
+          <Route path="/foundAnAnimal" element={<FoundAnimal/>} />
+          <Route path="/education" element={<Education/>} />
+          <Route path="/" element={<Landing/>} />
+          <Route path="/volunteer" element={<Volunteer/>} />
+          <Route path="/contact-us" element={<ContactUs/>} /> 
+          <Route path="*" element={<ErrorPage />} /> {/* Any other page which is not found above */}
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
