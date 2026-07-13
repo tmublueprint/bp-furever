6be4e10349e2d648b94d19eb45311f65620312d2
Zachary TN <104638755+ZacharyToyeN@users.noreply.github.com>
2026-07-12 21:33:03 -0400

    update more image + text + 2 new icons (#60)
    * added bucket
    
    * diff rewrites
    
    * test
    
    * logging
    
    * MORE LOGS
    
    * error handling on multer + fixing the url
    
    * direct file upload
    
    * direct file upload
    
    * backend working on local + removed bucket, working on getting firestore working
    
    * minor changes
    
    * apiUrled createPDF
    
    * correcting form names
    
    * Why was I not using JSON
    
    * further logging
    
    * apiUrled guide links
    
    * using properly storage urls
    
    * test
    
    * attempt api url
    
    * testing II
    
    * fix build failure
    
    * update firebase storage pattern
    
    * remove stupid logging
    
    * should be working
    
    * revert pathing
    
    * enabled cors
    
    * minor frontend fixes + education page now loads guides
    
    * facebook not used fix
    
    * add imgs
    
    * Added images
    
    * update more image + text

--- frontend/src/components/HowYouCanHelp/HowYouCanHelp.tsx
+++ frontend/src/components/HowYouCanHelp/HowYouCanHelp.tsx
@@ -2,10 +2,21 @@
 // import { Link, useNavigate } from "react-router-dom";
 // ^ May be required later
 import './HowYouCanHelp.css';
-import imagePlaceholder from '../../assets/HowYouCanHelp/how-you-can-help-card.svg'; 
 import volunteers from "../../assets/HowYouCanHelp/volunteers.jpg";
+import facebookSquirrel from "../../assets/HowYouCanHelp/facebookSquirrel.jpg";
+import guideExample from "../../assets/HowYouCanHelp/guideExample.jpg";
 
+const handleVolunteerClick = () => {
+    window.location.href = "/volunteer";
+};
 
+const handleLearnMoreClick = () => {
+    window.location.href = "/education";
+};
+
+const handleFacebookClick = () => {
+    window.open("https://www.facebook.com/FEVRWR/", "_blank");
+};
 
 function HowYouCanHelp() {
   return ( 
@@ -15,15 +26,15 @@ function HowYouCanHelp() {
         <div id="how-you-can-help">
             <div className="images">
                 <div style={{backgroundImage: `url(${volunteers})`, backgroundSize: `cover`}} aria-hidden = "true"></div>
-                <button className='how-you-can-help-btn-text'><p>Volunteer With Us</p></button>
+                <button className='how-you-can-help-btn-text' onClick={handleVolunteerClick}><p>Volunteer With Us</p></button>
             </div>
             <div className="images">
-                <div style={{backgroundImage: `url(${imagePlaceholder})`, backgroundSize: `cover`}} aria-hidden = "true" ></div>
-                <button className='how-you-can-help-btn-text'><p>Learn More</p></button>
+                <div style={{backgroundImage: `url(${guideExample})`, backgroundSize: `cover`}} aria-hidden = "true" id="guideExample"></div>
+                <button className='how-you-can-help-btn-text' onClick={handleLearnMoreClick}><p>Learn More</p></button>
             </div>
             <div className="images">
-                <div style={{backgroundImage: `url(${imagePlaceholder})`, backgroundSize: `cover`}} aria-hidden = "true"></div>
-                <button className='how-you-can-help-btn-text'><p>Follow Our Facebook</p></button>
+                <div style={{backgroundImage: `url(${facebookSquirrel})`, backgroundSize: `cover`}} aria-hidden = "true"></div>
+                <button className='how-you-can-help-btn-text' onClick={handleFacebookClick}><p>Follow Our Facebook</p></button>
 
             </div>
 
